9be6eb8acf44caca4786d5ee9e84d0cf5da3ad6a
abhay-a-ajoshi <abhay.joshi@synerzip.com>
2017-06-18 14:50:42 +0530

    dev server

---
+++ basics/dev-server/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+const paths = {
+    app: path.join(__dirname, 'app'),
+    build: path.join(__dirname, 'build')
+};
+
+const commonConfig = {
+    entry: {
+        app: paths.app
+    },
+    output: {
+        path: paths.build,
+        filename: '[name].js'
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            title: 'webpack level 1'
+        })
+    ]
+};
+
+const devServerConfig = {
+    devServer: {
+        inline: true,
+        historyApiFallback: true
+    }
+};
+
+module.exports = Object.assign({},
+    commonConfig,
+    devServerConfig
+);
